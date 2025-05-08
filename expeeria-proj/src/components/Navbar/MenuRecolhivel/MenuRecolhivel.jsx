671203211cb262c770c180417b5c5f79c859c196
Juan Pedro CMP <juanpedrocmp.15@gmail.com>
2025-05-08 06:49:33 -0300

    Mudanças no estilo

--- expeeria-proj/src/components/Navbar/MenuRecolhivel/MenuRecolhivel.jsx
+++ expeeria-proj/src/components/Navbar/MenuRecolhivel/MenuRecolhivel.jsx
@@ -42,11 +42,7 @@ const MenuRecolhivel = () => {
         className={style.dropdownButton}
         aria-label="Abrir menu"
       >
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <line x1="3" y1="12" x2="21" y2="12"></line>
-          <line x1="3" y1="6" x2="21" y2="6"></line>
-          <line x1="3" y1="18" x2="21" y2="18"></line>
-        </svg>
+     
       </button>
       
       {isOpen && (
@@ -68,10 +64,7 @@ const MenuRecolhivel = () => {
                 className={style.closeButton}
                 aria-label="Fechar menu"
               >
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                  <line x1="18" y1="6" x2="6" y2="18"></line>
-                  <line x1="6" y1="6" x2="18" y2="18"></line>
-                </svg>
+                X
               </button>
             </div>
             
